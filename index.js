536ad101dad33e131462be78eee22f433badbdb6
VortX87 <paul_jewkes@hotmail.co.uk>
2023-02-22 12:42:16 +0000

    all movies route done and working

--- index.js
+++ index.js
@@ -2,10 +2,35 @@ const express = require('express');
 const app = express();
 const {Movie} = require("./db/Movie")
 
+app.use(express.json())
+
 app.get("/movies", async(req, res) => {
     const movies = await Movie.findAll();
     res.send(movies);
 })
+
+app.post("/movies", async(req,res) =>{
+    const { title, genre, rating } = req.body
+    await Movie.create({title, genre, rating})
+    res.json(200)
+})
+
+app.delete("/movies/:id", async(req, res) => {
+    const { id } = req.params;
+    await Movie.destroy({
+        where: {id}
+    })
+    res.send(`Deleted Movie ${id}`)
+})
+
+app.put('/movies/:id', async (req, res) => {
+    const requestedId = req.params.id
+    const movie = await Movie.findOne({ where: {id:requestedId}})
+    movie.rating = req.body.rating;
+    await movie.save();
+    res.send(`Updated film`);
+});
+
 
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
